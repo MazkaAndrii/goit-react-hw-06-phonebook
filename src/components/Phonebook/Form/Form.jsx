981e68b420cc1456dd33533f1fa123ce97407dc4
MazkaAndrii <andriy.mazka@gmail.com>
2023-03-11 10:29:48 +0200

    refact all

--- src/components/Phonebook/Form/Form.jsx
+++ src/components/Phonebook/Form/Form.jsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { nanoid } from 'nanoid/non-secure';
+import { addContact, getContacts } from '../../../redux/slicer';
 import Input from '../Input/Input';
 import Label from '../Label/Label';
 import css from './Form.module.css';
 
-const Form = ({ onSubmit }) => {
+const Form = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+
+  const nameInputId = useMemo(() => nanoid(), []);
+  const numberInputId = useMemo(() => nanoid(), []);
+
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+
+  const formReset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+      ? alert(`${name} is already in contacts`)
+      : dispatch(addContact(newContact));
+    formReset();
+  };
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -21,20 +49,14 @@ const Form = ({ onSubmit }) => {
     }
   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    onSubmit({ name, number });
-    setName('');
-    setNumber('');
-  };
-
   return (
     <>
       <h1 className={css.mainTitle}>Phonebook</h1>
       <form className={css.form} onSubmit={handleSubmit}>
-        <Label text="Name">
+        <Label text="Name" htmlFor={nameInputId}>
           <Input
             type="text"
+            id={nameInputId}
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -42,10 +64,11 @@ const Form = ({ onSubmit }) => {
             onChange={handleChange}
           />
         </Label>
-        <Label text="Number">
+        <Label text="Number" htmlFor={numberInputId}>
           <Input
             type="tel"
             name="number"
+            id={numberInputId}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             value={number}
